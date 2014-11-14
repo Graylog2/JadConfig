c6cc5bab10f8732b8ffae188bd9a35d044d13044
Jochen Schalanda <jochen@schalanda.name>
2014-11-14 11:56:20 +0100

    Suppress unchecked warnings in FoobarConverterFactory

--- src/test/java/com/github/joschi/jadconfig/testconverters/FoobarConverterFactory.java
+++ src/test/java/com/github/joschi/jadconfig/testconverters/FoobarConverterFactory.java
@@ -3,14 +3,11 @@ package com.github.joschi.jadconfig.testconverters;
 import com.github.joschi.jadconfig.Converter;
 import com.github.joschi.jadconfig.ConverterFactory;
 
-/**
- * @author jschalanda
- */
 public class FoobarConverterFactory implements ConverterFactory {
-
     @Override
+    @SuppressWarnings("unchecked")
     public Class<? extends Converter<?>> getConverter(Class classType) {
-        if(classType == String.class) {
+        if (classType == String.class) {
             return FoobarConverter.class;
         }
 
