efb2ea706b4a6e72dde0fa6350a2e9081a0fd483
Jan Heise <jan.heise@graylog.com>
2025-10-06 11:39:55 +0200

    adding Documentation annotations

---
+++ src/main/java/com/github/joschi/jadconfig/documentation/DocumentationSection.java
@@ -0,0 +1,15 @@
+package com.github.joschi.jadconfig.documentation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE, ElementType.FIELD})
+public @interface DocumentationSection {
+    String heading();
+    String description();
+}
