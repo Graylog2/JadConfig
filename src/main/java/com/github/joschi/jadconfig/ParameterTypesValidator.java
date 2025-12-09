4afc68f508dd58a68610d54c0ff6c2c6d2907009
Tomas Dvorak <tomas.dvorak@graylog.com>
2025-12-09 07:36:13 +0100

    code cleanup

--- src/main/java/com/github/joschi/jadconfig/ParameterTypesValidator.java
+++ src/main/java/com/github/joschi/jadconfig/ParameterTypesValidator.java
@@ -31,33 +31,38 @@ public class ParameterTypesValidator extends AbstractProcessor {
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
         Types typeUtils = processingEnv.getTypeUtils();
 
-        for (TypeElement annotation : annotations) {
-            // Find elements annotated with MyCustomAnnotation
-            for (Element element : roundEnv.getElementsAnnotatedWith(annotation)) {
-                if (element.getKind().isField()) {
-                    VariableElement field = (VariableElement) element;
+        annotations.stream()
+                .flatMap(annotation -> roundEnv.getElementsAnnotatedWith(annotation).stream())
+                .map(element -> (VariableElement) element)
+                .forEach(element -> {processField(element,typeUtils);});
+        return false; // do not claim this annotation, let other processors handle it as well
+    }
 
-                    final String fieldName = getFieldName(field);
-                    TypeMirror fieldType = getBoxedType(typeUtils, field.asType());
+    private void processField(VariableElement field, Types typeUtils) {
+        final String fieldName = getFieldName(field);
+        TypeMirror fieldType = getBoxedType(typeUtils, field.asType());
 
-                    final AnnotationMirror annotationMirror = element.getAnnotationMirrors()
-                            .stream()
-                            .filter(mirror -> mirror.getAnnotationType().toString().equals(
-                                    Parameter.class.getCanonicalName())).findFirst()
-                            .orElseThrow(() -> new IllegalStateException("This should not happen"));
+        final AnnotationMirror annotationMirror = getParameterAnnotation(field);
+        final String parameterName = getParameterValue(annotationMirror);
 
-                    final String parameterName = annotationMirror.getElementValues().entrySet().stream()
-                            .filter(entry -> entry.getKey().getSimpleName().toString().equals("value"))
-                            .map(entry -> (String) entry.getValue().getValue())
-                            .findFirst()
-                            .orElseThrow(() -> new IllegalStateException("Value is mandatory!"));
+        verifyConverterType(annotationMirror, typeUtils, fieldType, parameterName, fieldName);
+        verifyValidators(annotationMirror, typeUtils, fieldType, parameterName, fieldName);
+    }
 
-                    verifyConverterType(annotationMirror, typeUtils, fieldType, parameterName, fieldName);
-                    verifyValidators(annotationMirror, typeUtils, fieldType, parameterName, fieldName);
-                }
-            }
-        }
-        return false;
+    private static String getParameterValue(AnnotationMirror annotationMirror) {
+        return annotationMirror.getElementValues().entrySet().stream()
+                .filter(entry -> entry.getKey().getSimpleName().toString().equals("value"))
+                .map(entry -> (String) entry.getValue().getValue())
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException("Value is mandatory!"));
+    }
+
+    private static AnnotationMirror getParameterAnnotation(Element element) {
+        return element.getAnnotationMirrors()
+                .stream()
+                .filter(mirror -> mirror.getAnnotationType().toString().equals(
+                        Parameter.class.getCanonicalName())).findFirst()
+                .orElseThrow(() -> new IllegalStateException("This should not happen, the element should be always annotated with Parameter annotation"));
     }
 
     private static String getFieldName(VariableElement field) {
@@ -76,23 +81,10 @@ public class ParameterTypesValidator extends AbstractProcessor {
     }
 
     private void verifyConverterType(AnnotationMirror annotationMirror, Types types, TypeMirror fieldType, String parameterName, String fieldName) {
-        final Optional<TypeMirror> converter = annotationMirror.getElementValues().entrySet().stream()
-                .filter(entry -> entry.getKey().getSimpleName().toString().equals("converter"))
-                .map(entry -> (TypeMirror) entry.getValue().getValue())
-                .findFirst();
-
-        converter.ifPresent(converterValue -> {
+        getConverter(annotationMirror).ifPresent(converterValue -> {
             TypeElement converterType = (TypeElement) types.asElement(converterValue);
-
-            List<? extends Element> members =
-                    processingEnv.getElementUtils().getAllMembers(converterType);
-
-            ExecutableElement convertFromMethod = members.stream()
-                    .filter(e -> e.getKind() == ElementKind.METHOD)
-                    .map(e -> (ExecutableElement) e)
-                    .filter(m -> m.getSimpleName().contentEquals("convertFrom"))
-                    .findFirst()
-                    .orElseThrow(() -> new IllegalStateException("This should not happen"));
+            List<? extends Element> members = processingEnv.getElementUtils().getAllMembers(converterType);
+            ExecutableElement convertFromMethod = getConvertFromMethod(members);
 
             final TypeMirror converterReturnType = convertFromMethod.getReturnType();
             if (!types.isSameType(converterReturnType, fieldType)) {
@@ -101,41 +93,58 @@ public class ParameterTypesValidator extends AbstractProcessor {
         });
     }
 
+    private static ExecutableElement getConvertFromMethod(List<? extends Element> members) {
+        return members.stream()
+                .filter(e -> e.getKind() == ElementKind.METHOD)
+                .map(e -> (ExecutableElement) e)
+                .filter(m -> m.getSimpleName().contentEquals("convertFrom"))
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException("This should not happen, converter should contain a convertFrom method"));
+    }
+
+    private static Optional<TypeMirror> getConverter(AnnotationMirror annotationMirror) {
+        return annotationMirror.getElementValues().entrySet().stream()
+                .filter(entry -> entry.getKey().getSimpleName().toString().equals("converter"))
+                .map(entry -> (TypeMirror) entry.getValue().getValue())
+                .findFirst();
+    }
+
     private void verifyValidators(AnnotationMirror annotationMirror, Types types, TypeMirror fieldType, String parameterName, String fieldName) {
         annotationMirror.getElementValues().entrySet().stream()
                 .filter(entry -> entry.getKey().getSimpleName().toString().equals("validators"))
                 .flatMap(entry -> {
-                    List<? extends AnnotationValue> values =
-                            (List<? extends AnnotationValue>) entry.getValue().getValue();
-                    return values.stream()
-                            .map(v -> (TypeMirror) v.getValue());
+                    List<? extends AnnotationValue> values = (List<? extends AnnotationValue>) entry.getValue().getValue();
+                    return values.stream() .map(v -> (TypeMirror) v.getValue());
                 })
                 .map(type -> (TypeElement) types.asElement(type))
-                .forEach(validatorType -> {
-                    List<? extends Element> members =
-                            processingEnv.getElementUtils().getAllMembers(validatorType);
+                .forEach(validatorType -> verifyValidator(types, fieldType, parameterName, fieldName, validatorType));
+    }
 
-                    ExecutableElement validatorMethod = members.stream()
-                            .filter(e -> e.getKind() == ElementKind.METHOD)
-                            .map(e -> (ExecutableElement) e)
-                            .filter(m -> m.getSimpleName().contentEquals("validate"))
-                            .findFirst()
-                            .orElseThrow(() -> new IllegalStateException("This should not happen"));
+    private void verifyValidator(Types types, TypeMirror fieldType, String parameterName, String fieldName, TypeElement validatorType) {
+        final ExecutableElement validatorMethod = getValidateMethod(validatorType);
+        final TypeMirror acceptedValidatorType = getValidatorType(types, validatorMethod);
 
-                    final TypeMirror acceptedValidatorType = getValidatorType(types, validatorMethod);
+        if (!types.isSameType(acceptedValidatorType, fieldType)) {
+            processingEnv.getMessager().printError("Property " + parameterName + " assigned to field " + fieldName + " has type " + fieldType + " but validator expects " + acceptedValidatorType);
+        }
+    }
 
-                    if (!types.isSameType(acceptedValidatorType, fieldType)) {
-                        processingEnv.getMessager().printError("Property " + parameterName + " assigned to field " + fieldName + " has type " + fieldType + " but validator expects " + acceptedValidatorType);
-                    }
-                });
+    private ExecutableElement getValidateMethod(TypeElement validatorType) {
+        List<? extends Element> members =
+                processingEnv.getElementUtils().getAllMembers(validatorType);
 
+        return members.stream()
+                .filter(e -> e.getKind() == ElementKind.METHOD)
+                .map(e -> (ExecutableElement) e)
+                .filter(m -> m.getSimpleName().contentEquals("validate"))
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException("This should not happen"));
     }
 
     private static TypeMirror getValidatorType(Types types, ExecutableElement convertFromMethod) {
         final List<? extends VariableElement> converterReturnType = convertFromMethod.getParameters();
         final TypeMirror acceptedValidatorType = converterReturnType.get(1).asType();
         return getBoxedType(types, acceptedValidatorType);
-
     }
 
     private static TypeMirror getBoxedType(Types types, TypeMirror type) {
