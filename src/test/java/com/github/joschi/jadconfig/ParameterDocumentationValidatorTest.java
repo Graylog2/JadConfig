bdb991fe2eeefefc9e72720f8a779986466643f4
Tomas Dvorak <tomas.dvorak@graylog.com>
2025-12-10 12:56:44 +0100

    Annotation processor for documentation enforcing

---
+++ src/test/java/com/github/joschi/jadconfig/ParameterDocumentationValidatorTest.java
@@ -0,0 +1,31 @@
+package com.github.joschi.jadconfig;
+
+import io.toolisticon.cute.Cute;
+import io.toolisticon.cute.CuteApi;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ParameterDocumentationValidatorTest {
+
+    CuteApi.BlackBoxTestSourceFilesInterface compileTestBuilder;
+
+    @BeforeEach
+    public void init() {
+        compileTestBuilder = Cute
+                .blackBoxTest()
+                .given()
+                .processor(ParameterDocumentationValidator.class);
+    }
+
+    @Test
+    public void testPropertyValidatorProcessor() {
+        compileTestBuilder
+                .andSourceFiles("/com/github/joschi/jadconfig/MyDocumentationValidatorTestClass.java")
+                .whenCompiled().thenExpectThat()
+                .compilationFails()
+                .andThat()
+                .compilerMessage()
+                .ofKindError().atLine(18).atColumn(22).equals("Property my_duration assigned to field MyDocumentationValidatorTestClass#myDurationField has no documentation available. Please, add @Documentation annotation!")
+                .executeTest();
+    }
+}
---
+++ src/test/resources/com/github/joschi/jadconfig/MyDocumentationValidatorTestClass.java
@@ -0,0 +1,20 @@
+import com.github.joschi.jadconfig.Parameter;
+import com.github.joschi.jadconfig.documentation.Documentation;
+
+import java.time.Duration;
+
+public class MyDocumentationValidatorTestClass {
+
+    @Documentation(visible = false)
+    @Parameter(value = "my_hidden_property")
+    private String hiddenProperty;
+
+    @Documentation("configure some int value")
+    @Parameter(value = "my_int")
+    private int myIntField = 10;
+
+    // here's no @Documentation annotation, should lead to an error
+    @Parameter(value = "my_duration")
+    private Duration myDurationField = Duration.ofDays(1);
+
+}
